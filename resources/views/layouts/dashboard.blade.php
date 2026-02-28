231288f6d9205b3ba687204c7522027764fc8555
countcipher <chrisharlow366@gmail.com>
2026-02-28 16:31:26 -0500

    Added in favicon

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -14,6 +14,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
 
+    <link rel="icon" href="/favicon1.ico" type="image/x-icon">
+
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
     @stack('head')
